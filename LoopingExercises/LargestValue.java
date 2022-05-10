940b711ccb3cf5510f04d991f99d5ac987925de3
quadrii <adisaquadri234@yahoo.com>
2022-05-10 08:25:35 +0100

    update on assignment

--- LoopingExercises/LargestValue.java
+++ LoopingExercises/LargestValue.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class LargestValue {
     public static void main(String[] arg){
         Scanner input = new Scanner(System.in);
-        LargestSales bestSeller = new LargestSales();
-        int unitSold;
+        int largestNumber = 0;
         int counter =1;
         while (counter <= 10){
             System.out.print("Enter Unit Sold: ");
-            unitSold = input.nextInt();
+            int unitSold = input.nextInt();
             counter = counter + 1;
+            if (unitSold > largestNumber){
+                largestNumber = unitSold;
+            }
         }
-        System.out.printf("The largest sales is %d%n: ", bestSeller.getLargestUnit());
+        System.out.printf("The largest sales is %d%n: ", largestNumber);
     }
 }
